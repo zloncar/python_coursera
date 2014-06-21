9468d00a9f3ba8167d691383d1791fc2fede9d01
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2014-06-21 09:12:01 +0100

    Format nicely.

--- ex_7_1.py
+++ ex_7_1.py
@@ -15,7 +15,7 @@ for i in fh:
     atpos = i.find(':')
     count = count + 1
     total_confidence = total_confidence + float(i[atpos+2:])
-    print 'count:', count, ', confidence:', float(i[atpos+2:]), ', total_confidence:', total_confidence
+    print 'count: %2d, conf: %5.4f, conf_sum: %7.4f' % (count, float(i[atpos+2:]), total_confidence)
 
 average_confidence = total_confidence / count
-print average_confidence
+print 'Average confidence = %06.4f' % (average_confidence)
